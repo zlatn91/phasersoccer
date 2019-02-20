6b20682cf3c4fd965f4426264f2e2d4c9cfd4d5a
Soozl <zlzltnn@gmail.com>
2019-02-20 17:14:36 +0900

    리팩토링 첫째날

--- WebContent/js/Main.js
+++ WebContent/js/Main.js
@@ -1,10 +1,16 @@
 window.onload = function() {
-	var game = new Phaser.Game(960, 540, Phaser.AUTO);
+	var game = new Phaser.Game(960, 540);
 
-	// Add the States your game has.
-	// game.state.add("Boot", Boot);
-	// game.state.add("Menu", Menu);
-	// game.state.add("Preload", Preload);
+//  Add the States your game has.
+//  game.state.add("Boot", Boot);
+//  game.state.add("Menu", Menu);
+//  game.state.add("Preload", Preload);
+
+//	var viewPort = document.getElementById('viewport');
+//	console.log(viewPort);
+//	viewPort.setAttribute('content', 'width=800');
+//	var canvas = document.getElementsByTagName(canvas);
+//	console.log(canvas);
 	game.state.add("Level", Level);
 
 	game.state.start("Level");

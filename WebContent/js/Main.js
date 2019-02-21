5ca81ce1cec724efb8b02a3d1b5a18d9575594e5
Soozl <zlzltnn@gmail.com>
2019-02-21 10:14:35 +0900

    Boot 추

--- WebContent/js/Main.js
+++ WebContent/js/Main.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-	var game = new Phaser.Game(960, 540);
+	var game = new Phaser.Game(960, 540, Phaser.AUTO);
 
 //  Add the States your game has.
 //  game.state.add("Boot", Boot);
@@ -11,7 +11,8 @@ window.onload = function() {
 //	viewPort.setAttribute('content', 'width=800');
 //	var canvas = document.getElementsByTagName(canvas);
 //	console.log(canvas);
+	game.state.add("Boot", Boot);
 	game.state.add("Level", Level);
-
+	
 	game.state.start("Level");
 };
